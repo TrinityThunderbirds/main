ef346e49d363662af02d2c51e347391aea306399
ChampPhil <109100465+ChampPhil@users.noreply.github.com>
2024-10-11 15:21:25 -0400

    Update opmode_template.java

--- opmode_template.java
+++ opmode_template.java
@@ -7,8 +7,24 @@ This is the base OPMODE template.
 */
 
 
+/* 
+Classes are Object Oriented Programming (OOP) code.
+They are a blueprint, and you can make specific instance(s) of 
+the 'blueprint'.
 
+Think of it like this: Car() is the class,
+and you can make different instances of the class,
+like a red car, green car, etc.
+*/
 public class MyFIRSTJavaOpMode extends LinearOpMode {
+    /*
+    Private attributes are class-specific variables that
+    cannot be accessed outside of the class.
+
+    In Java, we define/create private attributes
+    and then use public methods (which can be accessed outside)
+    to alter/access the private attirbutes
+    */
     private Gyroscope imu;
     private DcMotor motorTest;
     private DigitalChannel digitalTouch;
@@ -16,6 +32,11 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     private Servo servoTest;
 
 
+    /*
+    This a public function (accessible outside of the
+    class).
+    Void means it doesn't return anything
+    */
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(Gyroscope.class, "imu");
