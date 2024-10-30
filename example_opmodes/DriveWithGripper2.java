61e8eb57b9ed8b30419947296a395e3ccd06d765
Aviyan Panday <aviyanjp@gmail.com>
2024-10-30 19:06:19 -0400

    updated code it should work AFAIK

--- example_opmodes/DriveWithGripper2.java
+++ example_opmodes/DriveWithGripper2.java
@@ -8,7 +8,9 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="Drive Gripper 2", group="Exercises")
 //@Disabled
 public class DriveWithGripper2 extends LinearOpMode {
+public class DriveWithGripper2 extends LinearOpMode {
     DcMotor leftDrive, rightDrive, armMotor;
+    Servo intakeServo, wristServo;
     float leftX, leftY, rightY;
 
     boolean isTurning = false; // Flag to indicate if the robot is currently turning
@@ -16,10 +18,16 @@ public class DriveWithGripper2 extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException {
         leftDrive = hardwareMap.dcMotor.get("left_drive");
         rightDrive = hardwareMap.dcMotor.get("right_drive");
         armMotor = hardwareMap.dcMotor.get("arm_motor");
+        wristServo = hardwareMap.servo.get("wrist_servo");
+        intakeServo = hardwareMap.servo.get("intake_servo");
 
+        // Reverse motor directions
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
         // Reverse motor directions
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -27,6 +35,7 @@ public class DriveWithGripper2 extends LinearOpMode {
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
+        // Wait for start button
         // Wait for start button
         waitForStart();
 
@@ -37,6 +46,8 @@ public class DriveWithGripper2 extends LinearOpMode {
 
             // Right joystick for arm control (up/down)
             rightY = -gamepad1.right_stick_y; // Inverted for natural up/down motion
+
+            double wristServoPos = 0;
 
             // D-pad control for forward and backward
             if (gamepad1.dpad_up) {
@@ -66,11 +77,29 @@ public class DriveWithGripper2 extends LinearOpMode {
                 armMotor.setPower(0); // Stop arm if no triggers are pressed
             }
 
+            if (gamepad1.left_bumper && wristServoPos != 1) {
+                wristServoPos += 0.1;
+                wristServo.setPosition(wristServoPos);
+            }
+            if (gamepad1.right_bumper && wristServoPos != 0) {
+                wristServoPos -= 0.1;
+                wristServo.setPosition(wristServoPos);
+            }
+
+            if (gamepad1.y) {
+                intakeServo.setPosition(1);
+            }
+            if (gamepad1.a) {
+                intakeServo.setPosition(0);
+            }
+
             // Telemetry feedback
             telemetry.addData("left power", leftDrive.getPower());
             telemetry.addData("right power", rightDrive.getPower());
             telemetry.addData("arm power", armMotor.getPower());
             telemetry.update();
+
+            idle(); // Always call idle() at the end of each loop iteration
 
             idle(); // Always call idle() at the end of each loop iteration
         }
