4890e9ae0cc7d756f7560816238fc35f79d84cb5
flyingchamp14 <36345848+flyingchamp14@users.noreply.github.com>
2024-10-30 22:02:52 -0400

    Changed hardware map and added comments saying the controls

--- ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep (6).java
+++ ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep (6).java
@@ -1,24 +1,4 @@
-/*   MIT License
- *   Copyright (c) [2024] [Base 10 Assets, LLC]
- *
- *   Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
 
- *   The above copyright notice and this permission notice shall be included in all
- *   copies or substantial portions of the Software.
-
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *   SOFTWARE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -32,76 +12,58 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 /*
- * This OpMode is an example driver-controlled (TeleOp) mode for the goBILDA 2024-2025 FTC
- * Into The Deep Starter Robot
- * The code is structured as a LinearOpMode
  *
- * This robot has a two-motor differential-steered (sometimes called tank or skid steer) drivetrain.
- * With a left and right drive motor.
- * The drive on this robot is controlled in an "Arcade" style, with the left stick Y axis
+ * DRIVING STYLE: The drive on this robot is controlled in an "Arcade" style, with the left stick Y axis
  * controlling the forward movement and the right stick X axis controlling rotation.
  * This allows easy transition to a standard "First Person" control of a
  * mecanum or omnidirectional chassis.
  *
- * The drive wheels are 96mm diameter traction (Rhino) or omni wheels.
- * They are driven by 2x 5203-2402-0019 312RPM Yellow Jacket Planetary Gearmotors.
- *
- * This robot's main scoring mechanism includes an arm powered by a motor, a "wrist" driven
- * by a servo, and an intake driven by a continuous rotation servo.
- *
- * The arm is powered by a 5203-2402-0051 (50.9:1 Yellow Jacket Planetary Gearmotor) with an
- * external 5:1 reduction. This creates a total ~254.47:1 reduction.
- * This OpMode uses the motor's encoder and the RunToPosition method to drive the arm to
- * specific setpoints. These are defined as a number of degrees of rotation away from the arm's
- * starting position.
- *
- * Make super sure that the arm is reset into the robot, and the wrist is folded in before
+ * IMPORTANT: Make super sure that the arm is reset into the robot, and the wrist is folded in before
  * you run start the OpMode. The motor's encoder is "relative" and will move the number of degrees
  * you request it to based on the starting position. So if it starts too high, all the motor
  * setpoints will be wrong.
  *
- * The wrist is powered by a goBILDA Torque Servo (2000-0025-0002).
+ * MOST IMPORTANT
+ * SO IMPORTANT
+ * MUY IMPORTANT
+ * VVVVVVVVVVVVV
  *
- * The intake wheels are powered by a goBILDA Speed Servo (2000-0025-0003) in Continuous Rotation mode.
+ * CONTROLS:
+ * Left Joystick: Forward + Back MOTORS
+ * Right Joystick: Rotation MOTORS
+ * A: INTAKE collect
+ * X: INTAKE off
+ * B: INTAKE deposit
+ * RB: Intake sample ARM pos.
+ * LB: Pos. of ARM to clear barier
+ * Y: Low basket ARM pos.
+ * DpadLeft: WRIST folded, starting wrist pos.
+ * DpadRight: Specimin High Chamber WRIST Pos.
+ * DpadUp: ARM pos. to move to hook to hang.
+ * DpadDown: ARM moves to hang.
  */
 
-// In simple terms, this moves the grabber-arm to specified setpoints/locations.
-// This is done by controlling the # of degrees of rotation from the arm's starting rotation.
 
 @TeleOp(name="FTC Starter Kit Example Robot (INTO THE DEEP)", group="Robot")
 //@Disabled
 public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMode {
 
     /* Declare OpMode members. */
-    public DcMotor  leftDrive   = null; //the left drivetrain motor
-    public DcMotor  rightDrive  = null; //the right drivetrain motor
-    public DcMotor  armMotor    = null; //the arm motor
-    public CRServo  intake      = null; //the active intake servo
-    public Servo    wrist       = null; //the wrist servo
+   DcMotor leftDrive, rightDrive, armMotor;
+    Servo intakeServo, wristServo;
+    float leftX, leftY, rightY;
+    
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1/360.0; // we want ticks per degree, not per rotation
 
 
-    /* This constant is the number of encoder ticks for each degree of rotation of the arm.
-    To find this, we first need to consider the total gear reduction powering our arm.
-    First, we have an external 20t:100t (5:1) reduction created by two spur gears.
-    But we also have an internal gear reduction in our motor.
-    The motor we use for this arm is a 117RPM Yellow Jacket. Which has an internal gear
-    reduction of ~50.9:1. (more precisely it is 250047/4913:1)
-    We can multiply these two ratios together to get our final reduction of ~254.47:1.
-    The motor's encoder counts 28 times per rotation. So in total you should see about 7125.16
-    counts per rotation of the arm. We divide that by 360 to get the counts per degree. */
-    final double ARM_TICKS_PER_DEGREE = 19.7924893140647; //exact fraction is (194481/9826)
-
-
-    /* These constants hold the position that the arm is commanded to run to.
+    /* IMPORTANT: These constants hold the position that the arm is commanded to run to.
     These are relative to where the arm was located when you start the OpMode. So make sure the
     arm is reset to collapsed inside the robot before you start the program.
-
-    In these variables you'll see a number in degrees, multiplied by the ticks per degree of the arm.
-    This results in the number of encoder ticks the arm needs to move in order to achieve the ideal
-    set position of the arm. For example, the ARM_SCORE_SAMPLE_IN_LOW is set to
-    160 * ARM_TICKS_PER_DEGREE. This asks the arm to move 160° from the starting position.
-    If you'd like it to move further, increase that number. If you'd like it to not move
-    as far from the starting position, decrease it. */
+    */
 
     final double ARM_COLLAPSED_INTO_ROBOT  = 0;
     final double ARM_COLLECT               = 250 * ARM_TICKS_PER_DEGREE;
@@ -141,9 +103,9 @@ public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMod
 
 
         /* Define and Initialize Motors */
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_front_drive"); //the left drivetrain motor
-        rightDrive = hardwareMap.get(DcMotor.class, "right_front_drive"); //the right drivetrain motor
-        armMotor   = hardwareMap.get(DcMotor.class, "left_arm"); //the arm motor
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive"); //the left drivetrain motor
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive"); //the right drivetrain motor
+        armMotor   = hardwareMap.get(DcMotor.class, "arm_motor"); //the arm motor
 
 
         /* Most skid-steer/differential drive robots require reversing one motor to drive forward.
@@ -172,12 +134,12 @@ public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMod
 
 
         /* Define and initialize servos.*/
-        intake = hardwareMap.get(CRServo.class, "intake");
-        wrist  = hardwareMap.get(Servo.class, "wrist");
+        intakeServo = hardwareMap.get(CRServo.class, "intake_servo");
+        wristServo  = hardwareMap.get(Servo.class, "wrist_servo");
 
         /* Make sure that the intake is off, and the wrist is folded in. */
-        intake.setPower(INTAKE_OFF);
-        wrist.setPosition(WRIST_FOLDED_IN);
+        intakeServo.setPower(INTAKE_OFF);
+        wristServo.setPosition(WRIST_FOLDED_IN);
 
         /* Send telemetry message to signify robot waiting */
         telemetry.addLine("Robot Ready.");
@@ -225,37 +187,26 @@ public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMod
             If the user presses X, it sets the servo to Off.
             And if the user presses B it reveres the servo to spit out the element.*/
 
-            /* TECH TIP: If Else loops:
-            We're using an else if loop on "gamepad1.x" and "gamepad1.b" just in case
+            /* TECH TIP: If Else statements:
+            We're using an else if statement on "gamepad1.x" and "gamepad1.b" just in case
             multiple buttons are pressed at the same time. If the driver presses both "a" and "x"
             at the same time. "a" will win over and the intake will turn on. If we just had
             three if statements, then it will set the intake servo's power to multiple speeds in
             one cycle. Which can cause strange behavior. */
 
             if (gamepad1.a) {
-                intake.setPower(INTAKE_COLLECT);
+                intakeServo.setPower(INTAKE_COLLECT);
             }
             else if (gamepad1.x) {
-                intake.setPower(INTAKE_OFF);
+                intakeServo.setPower(INTAKE_OFF);
             }
             else if (gamepad1.b) {
-                intake.setPower(INTAKE_DEPOSIT);
+                intakeServo.setPower(INTAKE_DEPOSIT);
             }
 
 
-            /* Here we create a "fudge factor" for the arm position.
-            This allows you to adjust (or "fudge") the arm position slightly with the gamepad triggers.
-            We want the left trigger to move the arm up, and right trigger to move the arm down.
-            So we add the right trigger's variable to the inverse of the left trigger. If you pull
-            both triggers an equal amount, they cancel and leave the arm at zero. But if one is larger
-            than the other, it "wins out". This variable is then multiplied by our FUDGE_FACTOR.
-            The FUDGE_FACTOR is the number of degrees that we can adjust the arm by with this function. */
 
-            armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.right_trigger + (-gamepad1.left_trigger));
-
-
-
-            /* Here we implement a set of if else loops to set our arm to different scoring positions.
+            /* Here we implement a set of if else statements to set our arm to different scoring positions.
             We check to see if a specific button is pressed, and then move the arm (and sometimes
             intake and wrist) to match. For example, if we click the right bumper we want the robot
             to start collecting. So it moves the armPosition to the ARM_COLLECT position,
@@ -265,8 +216,8 @@ public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMod
             if(gamepad1.right_bumper){
                 /* This is the intaking/collecting arm position */
                 armPosition = ARM_COLLECT;
-                wrist.setPosition(WRIST_FOLDED_OUT);
-                intake.setPower(INTAKE_COLLECT);
+                wristServo.setPosition(WRIST_FOLDED_OUT);
+                intakeServo.setPower(INTAKE_COLLECT);
                 }
 
                 else if (gamepad1.left_bumper){
@@ -286,34 +237,46 @@ public class ConceptGoBildaStarterKitRobotTeleop_IntoTheDeep extends LinearOpMod
                     /* This turns off the intake, folds in the wrist, and moves the arm
                     back to folded inside the robot. This is also the starting configuration */
                     armPosition = ARM_COLLAPSED_INTO_ROBOT;
-                    intake.setPower(INTAKE_OFF);
-                    wrist.setPosition(WRIST_FOLDED_IN);
+                    intakeServo.setPower(INTAKE_OFF);
+                    wristServo.setPosition(WRIST_FOLDED_IN);
                 }
 
                 else if (gamepad1.dpad_right){
                     /* This is the correct height to score SPECIMEN on the HIGH CHAMBER */
                     armPosition = ARM_SCORE_SPECIMEN;
-                    wrist.setPosition(WRIST_FOLDED_IN);
+                    wristServo.setPosition(WRIST_FOLDED_IN);
                 }
 
                 else if (gamepad1.dpad_up){
                     /* This sets the arm to vertical to hook onto the LOW RUNG for hanging */
                     armPosition = ARM_ATTACH_HANGING_HOOK;
-                    intake.setPower(INTAKE_OFF);
-                    wrist.setPosition(WRIST_FOLDED_IN);
+                    intakeServo.setPower(INTAKE_OFF);
+                    wristServo.setPosition(WRIST_FOLDED_IN);
                 }
 
                 else if (gamepad1.dpad_down){
                     /* this moves the arm down to lift the robot up once it has been hooked */
                     armPosition = ARM_WINCH_ROBOT;
-                    intake.setPower(INTAKE_OFF);
-                    wrist.setPosition(WRIST_FOLDED_IN);
+                    intakeServo.setPower(INTAKE_OFF);
+                    wristServo.setPosition(WRIST_FOLDED_IN);
             }
+
+
+            /* Here we create a "fudge factor" for the arm position.
+            This allows you to adjust (or "fudge") the arm position slightly with the gamepad triggers.
+            We want the left trigger to move the arm up, and right trigger to move the arm down.
+            So we add the right trigger's variable to the inverse of the left trigger. If you pull
+            both triggers an equal amount, they cancel and leave the arm at zero. But if one is larger
+            than the other, it "wins out". This variable is then multiplied by our FUDGE_FACTOR.
+            The FUDGE_FACTOR is the number of degrees that we can adjust the arm by with this function. */
+
+            armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.right_trigger + (-gamepad1.left_trigger));
+
 
             /* Here we set the target position of our arm to match the variable that was selected
             by the driver.
             We also set the target velocity (speed) the motor runs at, and use setMode to run it.*/
-            armMotor.setTargetPosition((int) (armPosition  +armPositionFudgeFactor));
+            armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
 
             ((DcMotorEx) armMotor).setVelocity(2100);
             armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
