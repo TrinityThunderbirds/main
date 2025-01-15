7bd7fbeb584032a59c4e05db14d7a06cced81ea3
Aviyan Panday <aviyanjp@gmail.com>
2025-01-15 16:30:16 -0500

    raghav's request

--- example_opmodes/NewRobotTeleOp1.java
+++ example_opmodes/NewRobotTeleOp1.java
@@ -48,7 +48,7 @@ public class NewRobotTeleOp1 extends LinearOpMode {
         while (opModeIsActive()) {
             // Mecanum drive control
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
             // Denominator is the largest motor power (absolute value) or 1
